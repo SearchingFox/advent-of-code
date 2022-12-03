f8240d57a232ee6bb423ec0976c97524031040ba
SearchingFox <n3tstrang3r@yandex.ru>
2022-12-04 04:27:55 +1000

    Refactor days 1, 2 of 2022

--- 2022/Rust/src/day1.rs
+++ 2022/Rust/src/day1.rs
@@ -8,12 +8,12 @@ pub fn part_1(input: String) -> i32 {
 
 pub fn part_2(input: String) -> i32 {
     let mut output: Vec<i32> = input
-        .split("\n\n")
-        // .lines()
-        // .collect::<Vec<_>>()
-        // .group_by(|x, _| !x.is_empty())
-        .map(|x| x.lines().flat_map(|y| y.parse::<i32>()).sum())
+        .lines()
+        .collect::<Vec<_>>()
+        .group_by(|x, _| !x.is_empty())
+        .map(|x| x.iter().flat_map(|y| y.parse::<i32>()).sum())
         .collect::<Vec<_>>();
+
     output.sort();
     output.iter().rev().take(3).sum()
 }
--- 2022/Rust/src/day2.rs
+++ 2022/Rust/src/day2.rs
@@ -1,29 +1,29 @@
-pub fn part_1(input: String) -> i32 {
+pub fn part_1(input: &str) -> u32 {
     input
         .lines()
         .map(|line| {
-            let t: Vec<char> = line.chars().collect();
-            match t[2] as i32 - t[0] as i32 {
-                21 | 24 => 6 + t[2] as i32 - 87,
-                22 | 25 => t[2] as i32 - 87,
-                23 => 3 + t[2] as i32 - 87,
+            let t: Vec<u8> = line.chars().map(|x| x as u8).collect();
+            (match t[2] - t[0] {
+                21 | 24 => 6 + t[2] - 87,
+                22 | 25 => t[2] - 87,
+                23 => 3 + t[2] - 87,
                 _ => panic!("It can't be!"),
-            }
+            }) as u32
         })
         .sum()
 }
 
-pub fn part_2(input: String) -> i32 {
+pub fn part_2(input: &str) -> u32 {
     input
         .lines()
         .map(|line| {
-            let t: Vec<char> = line.chars().collect();
-            match t[2] {
-                'X' => t[0] as i32 % 3 + 1,
-                'Y' => 3 + t[0] as i32 - 64,
-                'Z' => 6 + (t[0] as i32 - 1) % 3 + 1,
+            let t: Vec<u8> = line.chars().map(|x| x as u8).collect();
+            (match t[2] {
+                88 => t[0] % 3 + 1,
+                89 => 3 + t[0] - 64,
+                90 => 6 + (t[0] - 1) % 3 + 1,
                 _ => panic!("It can't be!"),
-            }
+            }) as u32
         })
         .sum()
 }
