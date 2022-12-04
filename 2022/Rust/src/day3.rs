fbd7fddfe0e91b3c58e8022fa258217e8d42ed37
SearchingFox <n3tstrang3r@yandex.ru>
2022-12-05 06:30:52 +1000

    Refactor day 3 of 2022

--- 2022/Rust/src/day3.rs
+++ 2022/Rust/src/day3.rs
@@ -11,13 +11,15 @@ fn char_to_u32(c: char) -> u32 {
 pub fn part_1(input: &str) -> u32 {
     input
         .lines()
-        .flat_map(|line| {
+        .map(|line| {
             let (t1, t2) = line.split_at(line.len() / 2);
-            t1.chars()
-                .collect::<HashSet<_>>()
-                .intersection(&t2.chars().collect::<HashSet<_>>())
-                .map(|&x| char_to_u32(x))
-                .collect::<Vec<u32>>()
+            char_to_u32(
+                *t1.chars()
+                    .collect::<HashSet<_>>()
+                    .intersection(&t2.chars().collect::<HashSet<_>>())
+                    .next()
+                    .unwrap(),
+            )
         })
         .sum()
 }
@@ -28,13 +30,13 @@ pub fn part_2(input: &str) -> u32 {
         .array_chunks()
         .flat_map(|[x, y, z]| {
             let sets = [
-                x.chars().map(|c| char_to_u32(c)).collect::<HashSet<u32>>(),
-                y.chars().map(|c| char_to_u32(c)).collect::<HashSet<u32>>(),
-                z.chars().map(|c| char_to_u32(c)).collect::<HashSet<u32>>(),
+                x.chars().map(char_to_u32).collect::<HashSet<u32>>(),
+                y.chars().map(char_to_u32).collect::<HashSet<u32>>(),
+                z.chars().map(char_to_u32).collect::<HashSet<u32>>(),
             ];
 
             sets.iter().skip(1).fold(sets[0].clone(), |acc, hs| {
-                acc.intersection(&hs).cloned().collect()
+                acc.intersection(hs).cloned().collect()
             })
         })
         .sum()
