0a8fcdc7b4452622fa2ec391638317703cdc5178
SearchingFox <n3tstrang3r@yandex.ru>
2022-12-05 06:31:19 +1000

    Add day 4 of 2022

---
+++ 2022/Rust/src/day4.rs
@@ -0,0 +1,49 @@
+use std::collections::HashSet;
+
+fn str_to_u32((a, b): (&str, &str)) -> (u32, u32) {
+    (a.parse().unwrap(), b.parse().unwrap())
+}
+
+pub fn part_1(input: &str) -> usize {
+    input
+        .lines()
+        .filter(|line| {
+            let (p1, p2) = line.split_once(',').unwrap();
+            let (p1_start, p1_end) = str_to_u32(p1.split_once('-').unwrap());
+            let (p2_start, p2_end) = str_to_u32(p2.split_once('-').unwrap());
+
+            p1_start >= p2_start && p1_end <= p2_end || p2_start >= p1_start && p2_end <= p1_end
+        })
+        .count()
+}
+
+pub fn part_2(input: &str) -> usize {
+    input
+        .lines()
+        .filter(|line| {
+            let (p1, p2) = line.split_once(',').unwrap();
+            let (p1_start, p1_end) = str_to_u32(p1.split_once('-').unwrap());
+            let (p2_start, p2_end) = str_to_u32(p2.split_once('-').unwrap());
+
+            p1_start <= p2_start && p1_end >= p2_start || p2_start <= p1_start && p2_end >= p1_start
+        })
+        .count()
+}
+
+fn part_2_variant(input: &str) -> usize {
+    input
+        .lines()
+        .flat_map(|line| {
+            let str_to_u3 = |(a, b): (&str, &str)| -> _ {
+                (a.parse().unwrap()..=b.parse().unwrap()).collect::<HashSet<u32>>()
+            };
+            let (p1, p2) = line.split_once(',').unwrap();
+
+            let t = str_to_u3(p1.split_once('-').unwrap())
+                .intersection(&str_to_u3(p2.split_once('-').unwrap()))
+                .cloned()
+                .collect::<Vec<u32>>();
+            t.get(0).cloned()
+        })
+        .count()
+}
--- 2022/Rust/src/main.rs
+++ 2022/Rust/src/main.rs
@@ -4,9 +4,10 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
-    let input = std::fs::read_to_string("../input/day3.txt").unwrap();
-    println!("{:?}", day3::part_1(&input));
-    println!("{:?}", day3::part_2(&input));
+    let input = std::fs::read_to_string("../input/day4.txt").unwrap();
+    println!("{:?}", day4::part_1(&input));
+    println!("{:?}", day4::part_2(&input));
 }
