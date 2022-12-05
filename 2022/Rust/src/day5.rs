7ef4852a2462e5e789c53e8a3a3bc502cb252966
SearchingFox <n3tstrang3r@yandex.ru>
2022-12-05 23:32:06 +1000

    Add day 5 of 2022

---
+++ 2022/Rust/src/day5.rs
@@ -0,0 +1,61 @@
+use std::collections::BTreeMap;
+
+fn parse_containers(i: &str) -> BTreeMap<u8, Vec<char>> {
+    i.lines()
+        .flat_map(|line| {
+            line.chars()
+                .enumerate()
+                .flat_map(|(i, c)| c.is_ascii_alphabetic().then_some(((i / 4 + 1) as u8, c)))
+        })
+        .fold(BTreeMap::new(), |mut acc, (i, c)| {
+            acc.entry(i).and_modify(|x| x.push(c)).or_insert(vec![c]);
+            acc
+        })
+}
+
+pub fn part_1(input: &str) -> String {
+    let (t1, t2) = input.split_once("\r\n\r\n").unwrap();
+    let mut containers = parse_containers(t1);
+
+    for line in t2.lines() {
+        let t: Vec<&str> = line.split(' ').collect();
+        let size: usize = t[1].parse().unwrap();
+        let from: u8 = t[3].parse().unwrap();
+        let to: u8 = t[5].parse().unwrap();
+        let from_value = containers.get(&from).unwrap().clone();
+
+        containers.insert(from, from_value[size..].to_vec());
+        containers.insert(
+            to,
+            from_value[..size]
+                .iter()
+                .rev()
+                .chain(containers.get(&to).unwrap().iter())
+                .cloned()
+                .collect(),
+        );
+    }
+
+    containers.values().map(|v| v.first().unwrap()).cloned().collect::<String>()
+}
+
+pub fn part_2(input: &str) -> String {
+    let (t1, t2) = input.split_once("\r\n\r\n").unwrap();
+    let mut containers = parse_containers(t1);
+
+    for line in t2.lines() {
+        let t: Vec<&str> = line.split(' ').collect();
+        let size: usize = t[1].parse().unwrap();
+        let from: u8 = t[3].parse().unwrap();
+        let to: u8 = t[5].parse().unwrap();
+        let from_value = containers.get(&from).unwrap().clone();
+
+        containers.insert(from, from_value[size..].to_vec());
+        containers.insert(
+            to,
+            [&from_value[..size], containers.get(&to).unwrap()].concat(),
+        );
+    }
+
+    containers.values().map(|v| v.first().unwrap()).cloned().collect::<String>()
+}
--- 2022/Rust/src/main.rs
+++ 2022/Rust/src/main.rs
@@ -5,9 +5,10 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn main() {
-    let input = std::fs::read_to_string("../input/day4.txt").unwrap();
-    println!("{:?}", day4::part_1(&input));
-    println!("{:?}", day4::part_2(&input));
+    let input = std::fs::read_to_string("../input/day5.txt").unwrap();
+    println!("{:?}", day5::part_1(&input));
+    println!("{:?}", day5::part_2(&input));
 }
